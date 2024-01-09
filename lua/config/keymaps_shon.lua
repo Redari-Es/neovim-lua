79996ff29f3e3ea7eb2d8c8f5f100fbda492e812
Redari-Es <905422030@qq.com>
2024-01-10 04:09:13 +0800

    Update

--- lua/config/keymaps_shon.lua
+++ lua/config/keymaps_shon.lua
@@ -3,9 +3,9 @@ vim.g.maplocalleader = ' '
 
 -- 文件为go则设置;->:
 -- 检测文件类型为go,kt,xml
-vim.cmd([[autocmd FileType go,kt,xml inoremap ; :]])
-vim.cmd([[autocmd FileType go,kt,xml inoremap : ;]])
-vim.cmd([[autocmd FileType md set tw=80]])
+-- vim.cmd([[autocmd FileType go,kt,xml inoremap ; :]])
+-- vim.cmd([[autocmd FileType go,kt,xml inoremap : ;]])
+-- vim.cmd([[autocmd FileType md set tw=80]])
 
 
 local map = vim.api.nvim_set_keymap
@@ -19,3 +19,6 @@ map('', '\\s', ':%s//g<left><left>', opts)
 map('', 'tx', ':r !figlet', opts)
 -- Press space twice to jump to the next '    ' and edit it
 map('', '<LEADER><LEADER>', '<Esc>/<++><CR>:nohlsearch<CR>c4l', opts)
+-- for c language eof
+map('i', '<space>;', '<esc>A;<esc>o', opts)
+map('i', '<space><Tab>', '<esc>A<Tab><Tab><Tab><Tab>//', opts)
